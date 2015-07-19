b2b95d1ae8900cf0786ed2159047228be68b3ff1
navs7 <navneet7988@gmail.com>
2015-07-19 12:56:15 +0530

    Revert "test1"
    This reverts commit dc0a9f34def3a06a2993e97ffb1dec1eb4311499.

--- _/components/js/_myscript.js
+++
@@ -1,7 +0,0 @@
-/* ========================================================================
- * Bootstrap: transition.js v3.3.5
- * http://getbootstrap.com/javascript/#transitions
- * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
