dc0a9f34def3a06a2993e97ffb1dec1eb4311499
navs7 <navneet7988@gmail.com>
2015-07-19 12:55:15 +0530

    test1

---
+++ _/components/js/_myscript.js
@@ -0,0 +1,7 @@
+/* ========================================================================
+ * Bootstrap: transition.js v3.3.5
+ * http://getbootstrap.com/javascript/#transitions
+ * ========================================================================
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
